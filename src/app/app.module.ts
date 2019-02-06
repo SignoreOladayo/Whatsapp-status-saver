5cda9f8f36c74b57f9416291bd537bc49b9b42f5
Signore Oladayo <dannapel@gmail.com>
2019-02-07 00:58:40 +0100

    almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    File,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(){
+export class HomePage implements OnInit {
+
+  constructor(private file: File){
     
+  }
+
+  allMedia:any[] = []
+  allPhotos:any[] = []
+  photos:any[] = []
+  allVideos:any[] = []
+
+
+  ngOnInit(){
+    this.getMedia()
+  }
+
+  refresh() {
+    console.log('refresher')
+    this.getMedia()
+  }
+
+  setPhotos(){
+   const photoSetter = this.allPhotos.map(async(photo) => {
+        this.file.readAsDataURL(this.file.externalRootDirectory+'/WhatsApp/Media/.Statuses', photo.name)
+                 .then((url) => {
+                  this.photos.push({url,name:photo.name})
+                 })
+        
+    })
+   
+  }
+
+  getMedia() {
+    let root = this.file.externalRootDirectory
+    //open the directory
+    this.file.checkDir(root, 'WhatsApp/Media')
+             .then(()=> {
+                this.file.listDir(root, 'WhatsApp/Media/.Statuses')
+                         .then((files) => {
+                          const mediaMap = files.map(async (media) => {
+                             let extension = media.name.split('.').pop()
+                             if (extension === 'mp4') {
+                               this.allVideos.push(media)
+                             } else {
+                               this.allPhotos.push(media)
+                               
+                             }
+                           })
+
+                           Promise.all(mediaMap)
+                                  .then(()=> {
+                                    this.setPhotos()
+                                  })
+                         })
+                         .catch(err => console.log(err))
+             })
+             .catch(err => console.log(err))
+
+    //get all the files
+    
+    //sort images and save to array
+
+    //sort videos and save to vids array
   }
 }
